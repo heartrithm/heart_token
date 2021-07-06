6b8fb1baf567631d1cb1b80372cad440a02eb388
Nick Sullivan <nick@sullivanflock.com>
2021-07-06 17:51:49 -0600

    Add ERC20Votes from OpenZeppelin, with basic tests

--- contracts/HeartToken.sol
+++ contracts/HeartToken.sol
@@ -2,10 +2,94 @@
 
 pragma solidity 0.8.3;
 
-import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/access/AccessControlEnumerable.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/security/Pausable.sol";
 
-contract HeartToken is ERC20PresetMinterPauser {
-    constructor(uint256 initialSupply) ERC20PresetMinterPauser("HeartRithm", "HEART") {
+contract HeartToken is AccessControlEnumerable, ERC20VotesComp, Pausable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    constructor(uint256 initialSupply) ERC20("HeartRithm", "HEART") ERC20Permit("HeartRithm") {
+        // Assign the contract creator to all the roles
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, _msgSender());
+        _setupRole(PAUSER_ROLE, _msgSender());
+
+        // Mint the initial tokens
         _mint(msg.sender, initialSupply);
     }
+
+    /**
+     * @dev Creates `amount` new tokens for `to`.
+     *
+     * See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `MINTER_ROLE`.
+     */
+    function mint(address to, uint256 amount) public virtual {
+        require(hasRole(MINTER_ROLE, _msgSender()), "must have minter role to mint");
+        _mint(to, amount);
+    }
+
+
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for ``accounts``'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        unchecked {
+            _approve(account, _msgSender(), currentAllowance - amount);
+        }
+        _burn(account, amount);
+    }
+
+
+    /**
+     * @dev Pauses all token transfers.
+     *
+     * See {ERC20Pausable} and {Pausable-_pause}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the `PAUSER_ROLE`.
+     */
+    function pause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "must have pauser role to pause");
+        _pause();
+    }
+
+    function unpause() public virtual {
+        require(hasRole(PAUSER_ROLE, _msgSender()), "must have pauser role to unpause");
+        _unpause();
+    }
+
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        // Call the one from ERC20Votes 
+        super._beforeTokenTransfer(from, to, amount);
+
+        require(!paused(), "ERC20Pausable: token transfer while paused");
+
+    }
+
 }
