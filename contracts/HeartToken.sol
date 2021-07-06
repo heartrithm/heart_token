8bcae5f753b3b8643832f335c14b4ac0ac4421e9
Nick Sullivan <nick@sullivanflock.com>
2021-07-06 14:07:35 -0600

    Upgrade to openzeppelin 4.2

--- contracts/HeartToken.sol
+++ contracts/HeartToken.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.3;
 
-import "OpenZeppelin/openzeppelin-contracts@4.1.0/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.2.0/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract HeartToken is ERC20PresetMinterPauser {
     constructor(uint256 initialSupply) ERC20PresetMinterPauser("HeartRithm", "HEART") {
